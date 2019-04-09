76c766b40fa7534d68abad07e4d4c468a37ba3af
jmusau62-gmail <jmusau62@gmail.com>
2019-04-09 09:22:59 +0300

    added styles on css and included bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 $(document).ready(function() {
-  $("#roll").click(function player1() {
-    var josh = Math.floor(Math.random() * 6) + 1;
-    parseInt($("#total-1").text(josh));
-    if (josh != 1) {
-      total += josh;
-      parseInt($("#dice1").text(total));
-    } else {
-      total = 0;
-      parseInt($("#dice1").text(total));
-    }
-  });
-});
+    $("#roll").click(function playee1() {
+      var josh = Math.floor(Math.random() * 6) + 1;
+      parseInt($("#total-1").text(josh));
 
-/*second player*/
-$(document).ready(function() {
-$("#hold").click(function player2() {
-    var musau = Math.floor(Math.random() * 6) + 1;
-    parseInt($("#total-2").text(musau));
-    if (musau != 1) {
-      total += musau;
-      parseInt($("#dice2").text(total));
-    } else {
-      total = 0;
-      parseInt($)
-      "#dice2").text(total));
-}
-});
-});
+      if (josh != 1) {
+        total += josh;
+        parseInt($("#dice1").text(total));
+      } else {
+        total = 0;
+        parseInt($("#dice1").text(total));
+      }
+    });
+  });
+
+
+
+
+  /*second player*/
+
+
+  $(document).ready(function() {
+    $("#hold").click(function playee2() {
+      var musau = Math.floor(Math.random() * 6) + 1;
+      parseInt($("#total-2").text(musau));
+
+      if (musau != 1) {
+        total += musau;
+        parseInt($("#dice2").text(total));
+      } else {
+        total = 0;
+        parseInt($("#dice2").text(total));
+      }
+    });
+  });
